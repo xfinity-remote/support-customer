50477d9f0d1e596170d6ae7dd35d843fb464ca8d
Uk4-Bytes <useless8334@gmail.com>
2025-01-05 00:17:56 +0530

    fix:code cleanup

--- build.py
+++ build.py
@@ -296,8 +296,8 @@ Section: net
 Priority: optional
 Version: %s
 Architecture: %s
-Maintainer: xfinity <info@xfinity.com>
-Homepage: https://xfinity.com
+Maintainer: rustdesk <info@rustdesk.com>
+Homepage: https://rustdesk.com
 Depends: libgtk-3-0, libxcb-randr0, libxdo3, libxfixes3, libxcb-shape0, libxcb-xfixes0, libasound2, libsystemd0, curl, libva2, libva-drm2, libva-x11-2, libgstreamer-plugins-base1.0-0, libpam0g, gstreamer1.0-pipewire%s
 Recommends: libayatana-appindicator3-1
 Description: A remote control software.
